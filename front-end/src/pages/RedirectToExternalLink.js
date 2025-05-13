15236a627e82dad8398507d932f44ca0229e94ea
Khoi Nguyen <khoimegame69@gmail.com>
2025-05-13 09:30:12 +0700

    change menu price link

--- front-end/src/pages/RedirectToExternalLink.js
+++ front-end/src/pages/RedirectToExternalLink.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 
-const link = `https://photos.google.com/share/AF1QipPxOHyoZnReI6FzSaNXGZUXD2IqUabaRnIXjgyurc167hVWx1B-XAHMRmRRyeLDyg?key=UjFKSkJjUTJEM1NnT0JidXZWdk5ZTXJTUi1TNVFB`;
+const link = `https://photos.google.com/share/AF1QipNqFFcv0Rn1RWRiXc8_2CaE6IhM3WdURTQ0GloZ1G_ty76vx3Mcp_l39nHEqSHpVg?key=aHUzOXFuSG5rVE4zdVdDdWMwNUtMT2ZZRlM1VFl3`;
 const RedirectToExternalLink = () => {
   useEffect(() => {
     window.location.href = link;
